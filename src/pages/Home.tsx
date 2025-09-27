8fb92824cc89524c88f3a40474b5c1f439432937
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-27 23:27:24 +0700

    main two buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Search } from "lucide-react";
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <div className="text-black">Home Page</div>;
+  return (
+    <>
+      <div className="space-y-4">
+        <h1 className="text-2xl font-bold text-center mt-4">
+          Find Your Perfect Course
+        </h1>
+        <p className="text-center mt-4 text-gray-400">
+          Discover courses and professors through honest student reviews
+        </p>
+
+        {/* Search professor and course card */}
+        {/* <div className="border border-gray-300 rounded-lg p-4 mt-6 mx-auto w-full md:w-2/3 lg:w-1/2">
+          <div className="border border-gray-300 rounded-lg p-2 flex items-center gap-2">
+            <Search className="text-gray-400" />
+            <input
+              type="text"
+              placeholder="Search for professors or courses..."
+              className="w-full outline-none border-none focus:ring-0"
+            />
+          </div>
+          <div className="mt-4 flex justify-end"></div>
+        </div> */}
+
+        {/* Browse Buttons */}
+        <div className="flex justify-center gap-4 mt-4">
+          <Link to="/courses" className="flex justify-center mt-4">
+            <Button className="bg-[#8B0000] text-white px-4 py-2 rounded-lg hover:bg-[#a52a2a] transition">
+              Browse Courses
+            </Button>
+          </Link>
+          <Link to="/professors" className="flex justify-center mt-4">
+            <Button className=" text-[#8B0000] px-4 py-2 rounded-lg bg-white hover:bg-white border border-[#8B0000]">
+              Find Professors
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
