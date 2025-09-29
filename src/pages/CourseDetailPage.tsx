9aa01320ccda1232360fc3b7322cf29365a06a73
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-29 22:59:33 +0700

    course detail card finish

--- src/pages/CourseDetailPage.tsx
+++ src/pages/CourseDetailPage.tsx
@@ -12,12 +12,29 @@ export default function CourseDetailPage() {
     <>
       <Link to="/courses" className="flex items-center mt-4">
         <Button variant="ghost" className="px-0 text-[#8B0000]">
-          <ArrowLeft className="mr-2 h-4 w-4 text-[#8B0000]" />
+          <ArrowLeft className=" h-4 w-4 text-[#8B0000]" />
           Back to Courses
         </Button>
       </Link>
       {course ? (
-        <div></div>
+        <div className="border border-gray-300 rounded-lg p-6 mt-4 lg:w-3/4 sm:w-full">
+          <span className="text-3xl font-bold mb-2 mr-2">{course.code}</span>
+          <span className="text-2xl text-[#8B0000] font-semibold">
+            {course.name}
+          </span>
+          <span className="border border-gray-300 mt-4 rounded-md px-4 py-2 text-sm text-semibold ml-2 text-white bg-[#8B0000] items-end">
+            {course.credits} Credits
+          </span>
+
+          <h1 className="text-xl font-semibold mt-4 text-gray-500">
+            {course.faculty}
+          </h1>
+
+          <div className="border border-gray-300 rounded-md mt-6 p-6">
+            <h2 className="text-lg font-semibold">Course Description</h2>
+            <h2 className="text-gray-600 text-sm mt-3">{course.description}</h2>
+          </div>
+        </div>
       ) : (
         <p className="text-center text-gray-500 mt-6">Course not found.</p>
       )}
