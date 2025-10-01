b3d750d163cf559714f0defcec83f5041471b0b9
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-10-01 23:59:59 +0700

    detail page init

--- src/pages/Professors/professorDetailPage.tsx
+++ src/pages/Professors/professorDetailPage.tsx
@@ -1,3 +1,79 @@
+import { Button } from "@/components/ui/button";
+import { professors } from "@/data";
+import { ArrowLeft, GraduationCap, MapPin } from "lucide-react";
+import { Link, useParams } from "react-router-dom";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import StarRating from "@/components/page-components/StarRating";
 export default function professorDetailPage() {
-  return <div>professorDetailPage</div>;
+  const { professorId } = useParams();
+  const professor = professors.find((p) => p.id === Number(professorId));
+  return (
+    <>
+      {professor ? (
+        <div>
+          <Link to="/courses" className="flex items-center mt-4">
+            <Button variant="ghost" className="px-0 text-[#8B0000]">
+              <ArrowLeft className=" h-4 w-4 text-[#8B0000]" />
+              Back to Professors
+            </Button>
+          </Link>
+          <div className="lg:w-3/4 mt-6 sm:w-full">
+            <Card>
+              <CardHeader className="flex flex-col lg:flex-row gap-4 items-center">
+                <Avatar className="h-30 w-30">
+                  <AvatarImage src={professor.imageUrl} />
+                  <AvatarFallback className="bg-mfu-red text-white text-3xl">
+                    {professor.name
+                      ? professor.name
+                          .split(" ")
+                          .map((n: string) => n[0])
+                          .join("")
+                      : "P"}
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <CardTitle>{professor.name}</CardTitle>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <GraduationCap className="inline mr-1 h-5 w-5" />
+                    <p>{professor.faculty}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <MapPin className="inline mr-1 h-4 w-4" />
+                    <p>{professor.office}</p>
+                  </div>
+
+                  <div className="flex items-center gap-1 mt-1">
+                    <StarRating
+                      value={professor.averageRating ?? 0}
+                      readOnly={true}
+                    />
+                    <span>{professor.averageRating} / 5</span>
+                  </div>
+                </div>
+              </CardHeader>
+              <CardContent>
+                <p>Card Content</p>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      ) : (
+        <>
+          <p>Professor not found</p>
+        </>
+      )}
+    </>
+  );
 }
--- src/pages/Professors/professorlist.tsx
+++ src/pages/Professors/professorlist.tsx
@@ -20,6 +20,7 @@ import {
 } from "lucide-react";
 import StarRating from "@/components/page-components/StarRating";
 import { Separator } from "@/components/ui/separator";
+import { Link } from "react-router-dom";
 export interface ProfessorListPageProps {
   professors: Professor[];
 }
@@ -33,83 +34,85 @@ export default function ProfessorListPage({
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
           {professors.map((professor) => (
-            <Card key={professor.id}>
-              <CardHeader>
-                <div className="flex gap-2 items-center">
-                  <Avatar className="h-20 w-20">
-                    <AvatarImage src={professor.imageUrl} />
-                    <AvatarFallback className="bg-mfu-red text-white text-3xl">
-                      {professor.name
-                        ? professor.name
-                            .split(" ")
-                            .map((n: string) => n[0])
-                            .join("")
-                        : "P"}
-                    </AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <CardTitle>{professor.name}</CardTitle>
-                    <CardDescription>{professor.title}</CardDescription>
-                    <div className="flex items-center gap-1 mt-1">
-                      <StarRating
-                        value={professor?.averageRating ?? 0}
-                        readOnly={true}
-                      />
-                      <span>{professor.averageRating} / 5</span>
+            <Link key={professor.id} to={`/professors/${professor.id}`}>
+              <Card key={professor.id}>
+                <CardHeader>
+                  <div className="flex gap-2 items-center">
+                    <Avatar className="h-20 w-20">
+                      <AvatarImage src={professor.imageUrl} />
+                      <AvatarFallback className="bg-mfu-red text-white text-3xl">
+                        {professor.name
+                          ? professor.name
+                              .split(" ")
+                              .map((n: string) => n[0])
+                              .join("")
+                          : "P"}
+                      </AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <CardTitle>{professor.name}</CardTitle>
+                      <CardDescription>{professor.title}</CardDescription>
+                      <div className="flex items-center gap-1 mt-1">
+                        <StarRating
+                          value={professor?.averageRating ?? 0}
+                          readOnly={true}
+                        />
+                        <span>{professor.averageRating} / 5</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </CardHeader>
+                </CardHeader>
 
-              <CardContent>
-                <div className="flex items-center gap-1 text-gray-600">
-                  <GraduationCap className="inline mr-1 h-5 w-5" />
-                  <p>{professor.faculty}</p>
-                </div>
-                <div className="flex items-center gap-1 text-gray-600">
-                  <MapPin className="inline mr-1 h-4 w-4" />
-                  <p>{professor.office}</p>
-                </div>
-                <div className="flex items-center gap-1 text-gray-600">
-                  <Mail className="inline mr-1 h-4 w-4" />
-                  <p>{professor.email}</p>
-                </div>
-                <div className="flex items-center gap-1 text-gray-600">
-                  <PhoneCallIcon className="inline mr-1 h-4 w-4" />
-                  <p>{professor.phone}</p>
-                </div>
-              </CardContent>
-              {/* <Separator className="text-gray-600 w-[1px]" /> */}
-              <CardFooter className="flex flex-wrap gap-2 items-center">
-                <div>
-                  <p className="">Specializations: </p>
-                  {professor.specializations.map((spec, index) => (
-                    <span
-                      key={index}
-                      className="bg-gray-100 text-gray-800 text-sm px-2 py-1 rounded-lg pl-2 mr-2 mb-2 font-semibold"
-                    >
-                      {spec}
-                    </span>
-                  ))}
-                </div>
-
-                {/* Education */}
-                <div>
-                  <p className="">Education: </p>
-                  {professor.education.map((edu, index) => (
-                    <div>
-                      {/* <PrizeIcon className="inline mr-1 h-4 w-4"/> */}
-                      <h1
+                <CardContent>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <GraduationCap className="inline mr-1 h-5 w-5" />
+                    <p>{professor.faculty}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <MapPin className="inline mr-1 h-4 w-4" />
+                    <p>{professor.office}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <Mail className="inline mr-1 h-4 w-4" />
+                    <p>{professor.email}</p>
+                  </div>
+                  <div className="flex items-center gap-1 text-gray-600">
+                    <PhoneCallIcon className="inline mr-1 h-4 w-4" />
+                    <p>{professor.phone}</p>
+                  </div>
+                </CardContent>
+                {/* <Separator className="text-gray-600 w-[1px]" /> */}
+                <CardFooter className="flex flex-wrap gap-2 items-center">
+                  <div>
+                    <p className="">Specializations: </p>
+                    {professor.specializations.map((spec, index) => (
+                      <span
                         key={index}
-                        className=" text-sm px-2 py-1  pl-2 mr-1 mb-1"
+                        className="bg-gray-100 text-gray-800 text-sm px-2 py-1 rounded-lg pl-2 mr-2 mb-2 font-semibold"
                       >
-                        {edu}
-                      </h1>
-                    </div>
-                  ))}
-                </div>
-              </CardFooter>
-            </Card>
+                        {spec}
+                      </span>
+                    ))}
+                  </div>
+
+                  {/* Education */}
+                  <div>
+                    <p className="">Education: </p>
+                    {professor.education.map((edu, index) => (
+                      <div>
+                        {/* <PrizeIcon className="inline mr-1 h-4 w-4"/> */}
+                        <h1
+                          key={index}
+                          className=" text-sm px-2 py-1  pl-2 mr-1 mb-1"
+                        >
+                          {edu}
+                        </h1>
+                      </div>
+                    ))}
+                  </div>
+                </CardFooter>
+              </Card>
+            </Link>
           ))}
         </div>
       )}
