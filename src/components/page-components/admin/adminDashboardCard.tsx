12e83b93a078c93f90aa81aa59a1f48c6d86d2f9
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-10-04 20:03:48 +0700

    admin card finish

---
+++ src/components/page-components/admin/adminDashboardCard.tsx
@@ -0,0 +1,25 @@
+import { User, User2 } from "lucide-react";
+interface AdminDashboardCardProps {
+  title?: string;
+  description?: string;
+  icon?: React.ReactNode;
+}
+export default function AdminDashboardCard({
+  title,
+  description,
+  icon,
+}: AdminDashboardCardProps) {
+  return (
+    <>
+      <div className="flex mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="flex items-center justify-between bg-white p-4 h-[100px] rounded-xl border w-[300px]">
+          <div>
+            <h2 className="text-md text-gray-500 mb-2">{title}</h2>
+            <p className="text-3xl font-semibold">{description}</p>
+          </div>
+          {icon}
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/admin/adminDashboard.tsx
+++ src/pages/admin/adminDashboard.tsx
@@ -1,3 +1,43 @@
+import AdminDashboardCard from "@/components/page-components/admin/adminDashboardCard";
+import {
+  BookOpen,
+  GraduationCap,
+  MessageCircle,
+  User2,
+  UserCog,
+  UserSquare2,
+} from "lucide-react";
+
 export default function AdminDashboardPage() {
-  return <div>Admin Dashboard</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mt-4">Admin Dashboard</h1>
+      <p className="text-gray-500 mt-2">
+        Manage courses, professors, reviews, and view analytics
+      </p>
+
+      <div className="flex mt-6 grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-4">
+        <AdminDashboardCard
+          title="Total Students"
+          description="11111"
+          icon={<User2 className="text-[#8B0000]" size={40} />}
+        />
+        <AdminDashboardCard
+          title="Total Courses"
+          description="2222"
+          icon={<BookOpen className="text-[#8B0000]" size={40} />}
+        />
+        <AdminDashboardCard
+          title="Total Professors"
+          description="3333"
+          icon={<GraduationCap className="text-[#8B0000]" size={40} />}
+        />
+        <AdminDashboardCard
+          title="Total Reviews"
+          description="5678"
+          icon={<MessageCircle className="text-[#8B0000]" size={40} />}
+        />
+      </div>
+    </div>
+  );
 }
